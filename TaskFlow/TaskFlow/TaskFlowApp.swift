07b99183a2db59b6c39178b40300735918b5a1e8
İlknur Tulgar <ilknur001-@hotmail.com>
2025-10-24 18:36:48 +0300

    Project started

---
+++ TaskFlow/TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by İlknur Tulgar on 24.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ TaskFlow/TaskFlow/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  TaskFlow
+//
+//  Created by İlknur Tulgar on 24.10.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
